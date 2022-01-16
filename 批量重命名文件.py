f6d903ba7f3d28089171d59757f9ea7f76983eba
jiacx <jiacx@qq.com>
2022-01-16 22:23:48 +0800

    0

--- 批量重命名文件.py
+++ 批量重命名文件.py
@@ -7,10 +7,10 @@ def batch_rename(path, A):
     # 去掉A字符前面所有字段
     files = os.listdir(path)
     for f in files:
-        str = f[f.index(A):]
-        原来的文件路径 = os.path.join(path, f)
-        新的文件名 = os.path.join(path, str)
-        os.rename(原来的文件路径, 新的文件名)
+        新文件名 = f[f.index(A):]
+        原文件完整路径 = os.path.join(path, f)
+        新文件完整路径 = os.path.join(path, 新文件名)
+        os.rename(原文件完整路径, 新文件完整路径)
 
 # 测试
 # path = r"C:\Users\adminjia\Desktop\雪中悍刀行"
